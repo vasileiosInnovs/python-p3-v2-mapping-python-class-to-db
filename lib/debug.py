e099decfb6757368ddb3dd3a48beacb392231034
Hagee <hagee.ktr@outlook.com>
2025-05-21 13:01:10 +0300

    Code that manipulates tables

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,28 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print (payroll)
+
+payroll.save()
+print(payroll)
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)
+
+print("Delete Payroll")
+payroll.delete()
+print(payroll)
+
+hr.save()
+print(hr)
+
 ipdb.set_trace()
